4e5d19cdf41771809cd1fbeb57d4d5b460a1590f
Karol Piatek <karol.piatek@plasticity.cloud>
2026-04-15 03:39:15 +0000

    Add hostNetwork to cloud provider for IMDS access

--- eks-d-setup/07.5-install-cloud-provider.sh
+++ eks-d-setup/07.5-install-cloud-provider.sh
@@ -7,11 +7,15 @@ echo "Installing AWS Cloud Provider..."
 helm repo add aws-cloud-controller-manager https://kubernetes.github.io/cloud-provider-aws
 helm repo update
 
-# Install cloud-provider-aws
+# Get region from IMDS
+REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/region)
+
+# Install cloud-provider-aws with hostNetwork to access IMDS
 helm install aws-cloud-controller-manager \
   aws-cloud-controller-manager/aws-cloud-controller-manager \
   --namespace kube-system \
   --set nodeSelector."node-role\.kubernetes\.io/control-plane"="" \
+  --set hostNetwork=true \
   --set args[0]=--v=2 \
   --set args[1]=--cloud-provider=aws \
   --set args[2]=--use-service-account-credentials=true \
